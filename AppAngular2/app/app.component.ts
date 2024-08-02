80beec27c7283bc6cb4045ffdcd8c82c9501d228
adsAPP7 <102996757+adsAPP7@users.noreply.github.com>
2024-08-02 09:40:47 -0300

    Add project files.

---
+++ AppAngular2/app/app.component.ts
@@ -0,0 +1,7 @@
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'minha-app',
+    template: '<h1>Minha primeira aplicação Angular 2.0 no Visual Studio Code</h1><br>Macoratti .net'
+})
+export class MeuPrimeiroComponent_VSCode { }  
